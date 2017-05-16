e01b825a2c58f08f5fa034e043360ecf7f533d90
shruti-bh <bhosale.shruti18@gmail.com>
2017-05-16 08:09:14 -0700

    Q2 RNN NER

--- q2_rnn.py
+++ q2_rnn.py
@@ -61,7 +61,7 @@ class Config:
         self.log_output = self.output_path + "log"
 
 def pad_sequences(data, max_length):
-    """Ensures each input-output seqeunce pair in @data is of length
+    """Ensures each input-output sequence pair in @data is of length
     @max_length by padding it with zeros and truncating the rest of the
     sequence.
 
@@ -103,7 +103,14 @@ def pad_sequences(data, max_length):
 
     for sentence, labels in data:
         ### YOUR CODE HERE (~4-6 lines)
-        pass
+        padded_sentence = sentence[:max_length]
+        padded_labels = labels[:max_length]
+        while len(padded_sentence) < max_length:
+            padded_sentence.append(zero_vector)
+            padded_labels.append(zero_label)
+        true_length = min(len(sentence), max_length)
+        mask = [True] * true_length + [False] * (max_length - true_length)
+        ret.append((padded_sentence, padded_labels, mask))
         ### END YOUR CODE ###
     return ret
 
@@ -141,6 +148,10 @@ class RNNModel(NERModel):
         (Don't change the variable names)
         """
         ### YOUR CODE HERE (~4-6 lines)
+        self.input_placeholder = tf.placeholder(dtype=tf.int32, shape=[None, self.max_length, Config.n_features])
+        self.labels_placeholder = tf.placeholder(dtype=tf.int32, shape=[None, self.max_length])
+        self.mask_placeholder = tf.placeholder(dtype=tf.bool, shape=[None, self.max_length])
+        self.dropout_placeholder = tf.placeholder(dtype=tf.float32, shape=[])
         ### END YOUR CODE
 
     def create_feed_dict(self, inputs_batch, mask_batch, labels_batch=None, dropout=1):
@@ -166,6 +177,13 @@ class RNNModel(NERModel):
             feed_dict: The feed dictionary mapping from placeholders to values.
         """
         ### YOUR CODE (~6-10 lines)
+        feed_dict = {
+                self.input_placeholder: inputs_batch,
+                self.mask_placeholder: mask_batch,
+                self.dropout_placeholder: dropout
+        }
+        if labels_batch is not None:
+            feed_dict[self.labels_placeholder] = labels_batch 
         ### END YOUR CODE
         return feed_dict
 
@@ -190,6 +208,9 @@ class RNNModel(NERModel):
             embeddings: tf.Tensor of shape (None, max_length, n_features*embed_size)
         """
         ### YOUR CODE HERE (~4-6 lines)
+        L = tf.Variable(self.pretrained_embeddings)
+        embeddings = tf.nn.embedding_lookup(params=L, ids=self.input_placeholder)
+        embeddings = tf.reshape(embeddings, shape=[-1, self.max_length, Config.n_features * Config.embed_size])
         ### END YOUR CODE
         return embeddings
 
@@ -251,16 +272,26 @@ class RNNModel(NERModel):
         # Define U and b2 as variables.
         # Initialize state as vector of zeros.
         ### YOUR CODE HERE (~4-6 lines)
+        U = tf.get_variable("U", shape=[Config.hidden_size, Config.n_classes], initializer=tf.contrib.layers.xavier_initializer())
+        b2 = tf.get_variable("b2", shape=[Config.n_classes], initializer=tf.zeros_initializer())
+        state = tf.zeros(shape=[tf.shape(x)[0], Config.hidden_size])
         ### END YOUR CODE
-
         with tf.variable_scope("RNN"):
             for time_step in range(self.max_length):
-                ### YOUR CODE HERE (~6-10 lines)
-                pass
+                ### YOUR CODE HERE (~6-10 lines) 
+                if time_step > 0:
+                    tf.get_variable_scope().reuse_variables()
+                x_t = x[:, time_step, :]
+                o_t, h_t = cell(x_t, state)
+                o_drop_t = tf.nn.dropout(o_t, dropout_rate)
+                preds.append(tf.matmul(o_drop_t, U) + b2)
+                state = h_t
                 ### END YOUR CODE
 
         # Make sure to reshape @preds here.
         ### YOUR CODE HERE (~2-4 lines)
+        preds = tf.stack(preds) # T x None x n_classes
+        preds = tf.transpose(preds, perm=[1, 0, 2])
         ### END YOUR CODE
 
         assert preds.get_shape().as_list() == [None, self.max_length, self.config.n_classes], "predictions are not of the right shape. Expected {}, got {}".format([None, self.max_length, self.config.n_classes], preds.get_shape().as_list())
@@ -282,6 +313,7 @@ class RNNModel(NERModel):
             loss: A 0-d tensor (scalar)
         """
         ### YOUR CODE HERE (~2-4 lines)
+        loss = tf.reduce_mean(tf.boolean_mask(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.labels_placeholder, logits=preds), self.mask_placeholder))
         ### END YOUR CODE
         return loss
 
@@ -305,6 +337,8 @@ class RNNModel(NERModel):
             train_op: The Op for training.
         """
         ### YOUR CODE HERE (~1-2 lines)
+        optimizer = tf.train.AdamOptimizer(learning_rate=Config.lr)
+        train_op = optimizer.minimize(loss)
         ### END YOUR CODE
         return train_op
 
@@ -401,8 +435,9 @@ def do_test2(args):
 
         init = tf.global_variables_initializer()
         saver = None
-
-        with tf.Session() as session:
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        with tf.Session(config = config) as session:
             session.run(init)
             model.fit(session, saver, train, dev)
 
@@ -432,8 +467,10 @@ def do_train(args):
 
         init = tf.global_variables_initializer()
         saver = tf.train.Saver()
-
-        with tf.Session() as session:
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        config.gpu_options.per_process_gpu_memory_fraction = 0.99
+        with tf.Session(config=config) as session:
             session.run(init)
             model.fit(session, saver, train, dev)
             if report:
@@ -468,8 +505,9 @@ def do_evaluate(args):
 
         init = tf.global_variables_initializer()
         saver = tf.train.Saver()
-
-        with tf.Session() as session:
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        with tf.Session(config = config) as session:
             session.run(init)
             saver.restore(session, model.config.model_output)
             for sentence, labels, predictions in model.output(session, input_data):
@@ -490,8 +528,9 @@ def do_shell(args):
 
         init = tf.global_variables_initializer()
         saver = tf.train.Saver()
-
-        with tf.Session() as session:
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        with tf.Session(config = config) as session:
             session.run(init)
             saver.restore(session, model.config.model_output)
 
